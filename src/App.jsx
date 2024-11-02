62cdffb1d32f269fbb6e0b9396c3b9d06ee6ae26
kjunda01 <sinvaljuniorlms@gmail.com>
2024-11-02 10:42:15 -0300

    State Lift

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import "./App.css";
-import OutraLista from "./components/OutraLista";
+import SeuNome from "./components/SeuNome";
+import Saudacao from "./components/Saudacao";
 
 const App = () => {
-  const meusItens = ["React", "Vue", "Angular"];
+  const [nome, setNome] = useState()
 
   return (
     <div className="App">
-      <h1>Renderização de listas</h1>
-      <OutraLista itens={meusItens}/>
-      <OutraLista itens={[]}/>
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome}/>
     </div>
   );
 };
