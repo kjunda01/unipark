e1062d6d91a3ae7c1870df39af989c0477e2d252
kjunda01 <sinvaljuniorlms@gmail.com>
2024-11-02 14:03:20 -0300

    React Icons

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,16 @@
-function Footer(){
-    return (
-        <footer>
-            <p>Este é o rodapé</p>
-        </footer>
-    )
+import {FaFacebook, FaInstagram, FaLinkedin} from "react-icons/fa";
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <ul className={styles.socialList}>
+        <li><FaFacebook /></li>
+        <li><FaInstagram /></li>
+        <li><FaLinkedin /></li>
+      </ul>
+      <p>Nosso rodapé</p>
+    </footer>
+  );
 }
-export default Footer
+export default Footer;
