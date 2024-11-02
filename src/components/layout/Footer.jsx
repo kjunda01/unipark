83400df185887fca7a757b3f044b2f7c7774a82b
kjunda01 <sinvaljuniorlms@gmail.com>
2024-11-02 16:02:12 -0300

    Rodapé padrão 80%

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,15 +1,39 @@
-import {FaFacebook, FaInstagram, FaLinkedin} from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import styles from "./Footer.module.css";
+import MensagemFooter from "./MensagemFooter";
 
 function Footer() {
+  function paginaFacebook() {
+    location.replace("https://www.facebook.com/unilavras");
+  }
+  function paginaInstagram() {
+    location.replace("https://www.instagram.com/unilavras");
+  }
+  function paginaLinkedin() {
+    location.replace(
+      "https://br.linkedin.com/school/unilavras-centro-universitario/",
+    );
+  }
+
   return (
-    <footer>
-      <ul className={styles.socialList}>
-        <li><FaFacebook /></li>
-        <li><FaInstagram /></li>
-        <li><FaLinkedin /></li>
-      </ul>
-      <p>Nosso rodapé</p>
+    <footer className={styles.rodape}>
+      <div className={styles.imagemRodape}>
+        <p>Imagem</p>
+      </div>
+      <div className={styles.mensagemRodape}>
+        <ul className={styles.listaIcones}>
+          <li>
+            <FaFacebook onClick={paginaFacebook} />
+          </li>
+          <li>
+            <FaInstagram onClick={paginaInstagram} />
+          </li>
+          <li>
+            <FaLinkedin onClick={paginaLinkedin} />
+          </li>
+        </ul>
+        <MensagemFooter />
+      </div>
     </footer>
   );
 }
---
+++ src/components/layout/MensagemFooter.jsx
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+
+function MensagemFooter() {
+  return (
+    <div className={styles.mensagem}>
+      <p>UNILAVRAS - UNIPARK - Todos os direitos reservados</p>
+    </div>
+  );
+}
+export default MensagemFooter;
